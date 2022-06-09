4fe67a757d21e4ec13d6c9f12582d929cb1f475c
Dustin <dusmer@gmail.com>
2022-06-08 20:15:49 -0700

    added functionality to add books to library, as well as remove books from library

--- main.js
+++ main.js
@@ -12,10 +12,21 @@ myLibrary.push(yearBook);
 displayLibrary();
 
 
+
+
+
+
+
+//Listen for buttonclick and display form when clicked
 const addBookBtn = document.querySelector('#addBookOpen');
+const addBookSubmit = document.querySelector('#submit');
 addBookBtn.addEventListener('click', () => {
     formContainer.style.display = "block";
 });
+
+
+
+addBookSubmit.addEventListener('click', addBookToLibrary);
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -27,19 +38,45 @@ function Book(title, author, pages, read){
 
 
 function addBookToLibrary(){
+    const form = document.querySelector("#addBookForm");
+    const bookTitle = document.querySelector("#bookTitle");
+    const bookAuthor = document.querySelector("#bookAuthor");
+    const bookPages = document.querySelector("#bookPages");
+    const newBook = new Book(bookTitle.value, bookAuthor.value,bookPages.value,false);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    formContainer.style.display = "none";
+    form.reset();
+    displayLibrary();
+}
 
+function removeBookFromLibrary(index){
+    console.log(myLibrary);
+    myLibrary.splice(index,1);
+    displayLibrary();
 }
 
 function displayLibrary(){
     const container = document.querySelector('.library');
-
-    myLibrary.forEach(function(book){
+    //clear the library display area
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    
+    //Create a card for each book in the array
+    myLibrary.forEach(function(book, index){
         console.log(book);
         const bookCard = document.createElement('div');
         bookCard.classList.add('bookCard');
+        bookCard.setAttribute('data-position', `${index}`);   
         const title = document.createElement('p');
         const author = document.createElement('p');
         const pages = document.createElement('p');
+        const removeContainer = document.createElement('div');
+        const remove = document.createElement('button');
+        remove.classList.add('removeButton')
+        remove.textContent = "Remove";
+        remove.setAttribute('data-position', `${index}`); 
 
         title.textContent = `${book.title}`;
         author.textContent = `${book.author}`;
@@ -48,13 +85,22 @@ function displayLibrary(){
         bookCard.appendChild(title);
         bookCard.appendChild(author);
         bookCard.appendChild(pages);
+        removeContainer.appendChild(remove);
+        bookCard.appendChild(removeContainer);
 
-        /*const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = 'This is the glorious text-content!';*/
     
         container.appendChild(bookCard);
     })
+    const removeButtons = document.querySelectorAll('.removeButton');
+
+    removeButtons.forEach((button) => {
+
+        // and for each one we add a 'click' listener
+        button.addEventListener('click', () => {
+            console.log(button.dataset.position);
+            removeBookFromLibrary(button.dataset.position);
+        });
+    });
 
 
 }
